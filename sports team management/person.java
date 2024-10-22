47341a07748d2bd2a38577d8b257a13486fe62c1
Seri Nur Ayuni <147372910+serinrayuni@users.noreply.github.com>
2024-10-22 23:32:57 +0800

    Create person.java

---
+++ sports team management/person.java
@@ -0,0 +1,26 @@
+class Person {
+    private String name, gender;
+    private int age;
+
+    public Person(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getInfo() {
+        return "Name: " + name + ", Age: " + age + "\nGender: " + gender;
+    }
+}
