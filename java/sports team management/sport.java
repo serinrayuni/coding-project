56bd07707b7f16b789b05c760e563d496dc9e5e4
Seri Nur Ayuni <147372910+serinrayuni@users.noreply.github.com>
2024-10-22 23:37:18 +0800

    Create sport.java

---
+++ java/sports team management/sport.java
@@ -0,0 +1,21 @@
+class Sport {
+    private String sportName, category;
+
+    public Sport(String sportName, String category) {
+        this.sportName = sportName;
+        this.category = category;
+    }
+
+    public String getSportName() {
+        return sportName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String toString() {
+        return "Sport Name: " + sportName + "\nCategory: " + category;
+    }
+
+}
